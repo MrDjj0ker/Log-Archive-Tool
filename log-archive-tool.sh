acc8d5ec20b3e3d0d34f40d49bb2e5212e6c8c9a
root <aghababayevarif@gmail.com>
2025-10-18 21:12:15 +0400

    maybe last commit

--- log-archive-tool.sh
+++ log-archive-tool.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+set -euo pipefail
+LOG_ARCHIVE=/var/log/xrdp.log
+prevarchive=/root/logforserver/log_archive_`date +"%H-%M-%S-%d-%m-%Y"`
+cat $LOG_ARCHIVE >> $prevarchive
+tar -cvzf $prevarchive.tar.gz $prevarchive
+rm -f $prevarchive
+
